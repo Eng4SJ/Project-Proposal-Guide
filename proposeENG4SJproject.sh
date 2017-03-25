0f62ac19a3ff949fde142184c1ffc17c80af311c
haniawni <hani.awni@gmail.com>
2017-03-24 20:39:17 -0500

    fix minor convenience bugs

--- proposeENG4SJproject.sh
+++ proposeENG4SJproject.sh
@@ -50,10 +50,12 @@ cd $ENG4SJ_TLDir
 mkdir $REPONAME
 cd $REPONAME
 git init
-echo '# $REPONAME Project Proposal' > readme.md
+echo "# $REPONAME Project Proposal" > readme.md
 curl https://raw.githubusercontent.com/Eng4SJ/Project-Proposal-Guide/master/template-readme.md >> readme.md
 git add readme.md
 git commit -m "init commit"
 git remote add origin https://github.com/ENG4SJ/$REPONAME.git
 git push --set-upstream origin master
+cd ..
 subl .
+
